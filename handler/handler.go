9d0e66b62c1bb419a0b0c1045b828df5fcb44074
fishqiudao <fishqiudao@gmail.com>
2025-03-06 21:29:46 +0800

    03-06

--- handler/handler.go
+++ handler/handler.go
@@ -1,10 +1,14 @@
 package handler
 
 import (
+	"encoding/json"
+	"filestore-server/meta"
+	"filestore-server/util"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 // 处理文件上传
@@ -26,18 +30,28 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
-		newFile, err := os.Create("./cloud/" + head.Filename)
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "./cloud/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Printf("Failed to create file, err:%s\n", err.Error())
 			return
 		}
 		defer newFile.Close()
 
-		_, err = io.Copy(newFile, file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			fmt.Printf("Failed to save data into file, err:%s\n", err.Error())
 			return
 		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileShal = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
 
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
@@ -46,4 +60,100 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 // 显示上传成功页面
 func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Upload finished!")
+}
+
+// 获取文件元信息
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	// filehash := r.Form["filehash"][0]
+	filehash := r.Form.Get("filehash")
+	fMeta := meta.GetFileMeta(filehash)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+// 下载网盘文件
+func Downloadhandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filehash := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(filehash)
+	if fm.FileName == "" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("File not found"))
+		return
+	}
+	// fmt.Println("获取文件信息成功")
+
+	// fmt.Println(fm.Location)
+
+	// fmt.Println("文件存在")
+
+	f, err := os.Open(fm.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// fmt.Println("打开文件成功")
+
+	defer f.Close()
+	data, err := io.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// fmt.Println("读取文件成功")
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", "attachment;filename=\""+fm.FileName+"\"")
+	w.Write(data)
+	// fmt.Println("下载文件成功")
+
+}
+
+// 更新文件元信息接口(重命名)
+func FileMetaUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	opType := r.Form.Get("op") //0表示重命名
+	filehash := r.Form.Get("filehash")
+	newFileName := r.Form.Get("filename")
+
+	if opType != "0" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	curFileMeta := meta.GetFileMeta(filehash)
+
+	os.Rename(curFileMeta.Location, "./cloud/"+newFileName) //重命名物理文件
+	curFileMeta.Location = "./cloud/" + newFileName
+	curFileMeta.FileName = newFileName //更新元信息（逻辑文件信息）
+	meta.UpdateFileMeta(curFileMeta)
+
+	data, err := json.Marshal(curFileMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+// FileDeleteHandler 删除文件及元信息
+func FileDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filehash := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(filehash)
+	os.Remove(fm.Location)        //删除物理文件
+	meta.RemoveFileMeta(filehash) //删除元信息（逻辑文件）
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("删除成功"))
 }
